6633851b09e716bca3a91dcd490229977ba31d3b
larakev <kevin.lara@mail.ru>
2017-02-27 09:42:11 -0600

    update

--- web/add.php
+++ web/add.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 header('Content-Type: text/html; charset=ISO-8859-1');
 
@@ -27,36 +26,4 @@ if(isset($_POST['save'])) {
 		print "<script>alert(\"Error\");window.location='../';</script>";
 	}
 	
-}else {}
-=======
-<?php
-header('Content-Type: text/html; charset=ISO-8859-1');
-
-  $server = "localhost";
-  $username = "root";
-  $password = "";
-  $database = "maps";
-  
-  error_reporting(E_COMPILE_ERROR|E_ERROR|E_CORE_ERROR);
-  mysql_connect($server, $username, $password) or die("No se encontro el servidor");
-  mysql_select_db($database) or die("No se encontro la base de datos");
-  mysql_set_charset('utf8');
-
-if(isset($_POST['save'])) {
-	$name = mysql_real_escape_string($_POST['name']);
-	$address = mysql_real_escape_string($_POST['address']);
-	$latitude = mysql_real_escape_string($_POST['latitude']);
-	$longitude = mysql_real_escape_string($_POST['longitude']);
-	// $type = mysql_real_escape_string($_POST['type']);
-	
-	$reg = mysql_query("INSERT INTO markers (name, address, lat, lng) 
-		VALUES ('".$name."', '".$address."', '".$latitude."', '".$longitude."')")or die(mysql_error());
-	if($reg) {
-		print "<script>alert(\"Successful.\");window.location='../';</script>";
-	}else {
-		print "<script>alert(\"Error\");window.location='../';</script>";
-	}
-	
-}else {}
->>>>>>> origin/master
-?>
+}
